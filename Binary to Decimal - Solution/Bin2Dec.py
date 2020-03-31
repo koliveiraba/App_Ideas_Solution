a80e125270ffd1639b4af20f55ee61854316da57
Klismann de Oliveira Barros <52436637+KlismannB@users.noreply.github.com>
2020-03-31 02:01:37 -0300

    Add files via upload

---
+++ Binary to Decimal - Solution/Bin2Dec.py
@@ -0,0 +1,12 @@
+binaryNumber = input("Type a binary number (any size): ") # recives a binary number as a string and stores at the variable 'binaryNumber'
+
+value = 0 # This is the value that will be printed as the decimal number
+times = 0 # This is the power that the 2 number will be raised
+
+for number in binaryNumber[::-1]: # This for loop will get each 0 or 1 number
+    if(number == ' '): 
+        continue # Ignore's the charactere ' '
+    value += int(number)*(2**(times)) # that number (1 or 0) will be multiplied by and eraised to the 'times' power and will be stored at the variable 'value' until the end of the string
+    times += 1 # that way we can guarantee that the number will be raised to a different power each complete cicle of the for loop
+    
+print ("The binary number: {} corresponds to the decimal number: {}" .format(binaryNumber,value)) # That printed value will be the corresponding decimal number from that binary number recived
